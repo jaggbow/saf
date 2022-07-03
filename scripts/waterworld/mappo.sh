adda05aafb1e5db95607dd387f34c24959ce2ff5
jaggbow <oussama.boussif@student.ecp.fr>
2022-07-03 10:36:38 -0400

    fixed name of variable from N_agents to n_agents in waterworld

--- scripts/waterworld/mappo.sh
+++ scripts/waterworld/mappo.sh
@@ -6,8 +6,11 @@
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --mem=60G                                     
 #SBATCH --time=16:00:00
-#SBATCH -o /network/scratch/o/oussama.boussif/slurms/mappo_waterworld-slurm-%j.out  
+#SBATCH --array=1-10
+#SBATCH -o /network/scratch/o/oussama.boussif/slurms/mappo_waterworld-slurm-%A_%a.out  
 
+param_store=scripts/seeds.txt
+seed=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
 # 1. Load the required modules
 module --quiet load anaconda/3
 conda activate marl
@@ -19,5 +22,6 @@ runner.params.lr_decay=False \
 runner.params.comet.project_name=waterworld \
 runner.params.total_timesteps=10000000 \
 n_agents=5 \
+seed=$seed \
 continuous_action=True \
 env_steps=500
