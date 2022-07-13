15f022c8fc200a1a1d6b1101328c812ef88902a4
jaggbow <oussama.boussif@student.ecp.fr>
2022-07-13 17:22:15 -0400

    added scripts for running algos on different starcraft maps

---
+++ scripts/starcraft/mappo_25m.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=mappo_25m
+#SBATCH --partition=long                        
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=60G                                     
+#SBATCH --time=24:00:00
+#SBATCH --array=1-10
+#SBATCH -o /network/scratch/o/oussama.boussif/slurms/mappo_25m-slurm-%A_%a.out
+
+param_store=scripts/seeds.txt
+seed=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
+# 1. Load the required modules
+module --quiet load anaconda/3
+conda activate marl
+
+python run.py \
+policy=mappo \
+env=starcraft \
+env.name=25m \
+runner.params.lr_decay=False \
+runner.params.comet.project_name=starcraft \
+runner.params.total_timesteps=10000000 \
+n_agents=25 \
+seed=$seed \
+continuous_action=False \
+env_steps=150
---
+++ scripts/starcraft/mappo_8m.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=mappo_8m
+#SBATCH --partition=long                        
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=60G                                     
+#SBATCH --time=24:00:00
+#SBATCH --array=1-10
+#SBATCH -o /network/scratch/o/oussama.boussif/slurms/mappo_8m-slurm-%A_%a.out
+
+param_store=scripts/seeds.txt
+seed=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
+# 1. Load the required modules
+module --quiet load anaconda/3
+conda activate marl
+
+python run.py \
+policy=mappo \
+env=starcraft \
+env.name=8m \
+runner.params.lr_decay=False \
+runner.params.comet.project_name=starcraft \
+runner.params.total_timesteps=10000000 \
+n_agents=8 \
+seed=$seed \
+continuous_action=False \
+env_steps=120
